fdc292153e35ed740aec1f01fbbc3ca0566e43c5
Hoang Le <artstyle0416@gmail.com>
2021-07-15 02:10:16 +0700

    finish basic CRUD

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
 const path = require("path");
 const express = require("express");
+const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const morgan = require("morgan");
 const exphbs = require("express-handlebars");
+const methodOverride = require("method-override");
 const passport = require("passport");
 const session = require("express-session");
+const MongoDbStore = require("connect-mongo");
 const connectDB = require("./config/db");
-const { Cookie } = require("express-session");
+// const { Cookie } = require("express-session");
 
 //Load config
 dotenv.config({ path: "./config/config.env" });
@@ -18,13 +21,45 @@ connectDB();
 
 const app = express();
 
+// Body parser
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+//Method override
+app.use(
+  methodOverride(function (req, res) {
+    if (req.body && typeof req.body === "object" && "_method" in req.body) {
+      // look in urlencoded POST bodies and delete it
+      let method = req.body._method;
+      delete req.body._method;
+      return method;
+    }
+  })
+);
+
 //Logging
 if (process.env.NODE_ENV === "development") {
   app.use(morgan("dev"));
 }
 
+//Handlebars Helpers
+const {
+  formatDate,
+  stripTags,
+  truncate,
+  editIcon,
+  select,
+} = require("./helpers/hbs");
+
 //Handlebars
-app.engine(".hbs", exphbs({ defaultLayout: "main", extname: ".hbs" }));
+app.engine(
+  ".hbs",
+  exphbs({
+    helpers: { formatDate, stripTags, truncate, editIcon, select },
+    defaultLayout: "main",
+    extname: ".hbs",
+  })
+);
 app.set("view engine", ".hbs");
 
 //Sessions
@@ -33,6 +68,10 @@ app.use(
     secret: "keyboard cat",
     resave: false,
     saveUninitialized: false,
+    store: MongoDbStore.create({
+      mongoUrl:
+        "mongodb+srv://hoangulrich:hello123@cluster0.6fhze.mongodb.net/storyline?retryWrites=true&w=majority",
+    }),
     // cookie: { secure: true },
   })
 );
@@ -41,12 +80,19 @@ app.use(
 app.use(passport.initialize());
 app.use(passport.session());
 
+//Set Global Variable
+app.use(function (req, res, next) {
+  res.locals.user = req.user || null;
+  next();
+});
+
 //Static folder
 app.use(express.static(path.join(__dirname, "public")));
 
 //Routes
 app.use("/", require("./routes/index"));
 app.use("/auth", require("./routes/auth"));
+app.use("/stories", require("./routes/stories"));
 
 const PORT = process.env.PORT || 3000;
 
